dfbc26cbd8d1cf686d33a64c7669596404c09b0e
Dorivaldo Valentim <dorivaldovalentim@gmail.com>
2021-01-27 20:28:13 +0100

    Criando as rotas correspondentes a cada método criado anteriormente

--- resources/views/components/main-menu.blade.php
+++ resources/views/components/main-menu.blade.php
@@ -4,24 +4,19 @@
         <div class="container">
             <div class="row">
                 <div class="col-xs-2">
-                    <div id="colorlib-logo"><a href="index.html">Complex</a></div>
+                    <div id="colorlib-logo">
+                        <a href="{{ route('home') }}">Complex</a>
+                    </div>
                 </div>
+
                 <div class="col-xs-10 text-right menu-1">
                     <ul>
-                        <li class="active"><a href="index.html">Home</a></li>
-                        <li class="has-dropdown">
-                            <a href="project.html">Projects</a>
-                            <ul class="dropdown">
-                                <li><a href="#">Commercial</a></li>
-                                <li><a href="#">Apartment</a></li>
-                                <li><a href="#">House</a></li>
-                                <li><a href="#">Building</a></li>
-                            </ul>
-                        </li>
-                        <li><a href="services.html">Services</a></li>
-                        <li><a href="blog.html">Blog</a></li>
-                        <li><a href="about.html">About</a></li>
-                        <li><a href="contact.html">Contact</a></li>
+                        <li><a href="{{ route('home') }}">Página Inicial</a></li>
+                        <li><a href="{{ route('projects') }}">Projectos</a></li>
+                        <li><a href="{{ route('services') }}">Serviços</a></li>
+                        <li><a href="{{ route('blog') }}">Blog</a></li>
+                        <li><a href="{{ route('about') }}">Sobre</a></li>
+                        <li><a href="{{ route('contacts') }}">Contactos</a></li>
                     </ul>
                 </div>
             </div>
