4338747280cf1e938c93c4b61e1f88ce8cd11895
Dorivaldo Valentim <dorivaldovalentim@gmail.com>
2021-01-27 23:19:59 +0100

    Adicionando o content do main section na home

--- resources/views/frontsite/home.blade.php
+++ resources/views/frontsite/home.blade.php
@@ -76,4 +76,151 @@
 
     </aside>
 
+    <div id="colorlib-intro">
+
+        <div class="container">
+
+            <div class="row">
+
+                <div class="col-md-12 tabulation animate-box">
+
+                    <ul class="nav nav-tabs">
+                        <li class="active">
+                            <a data-toggle="tab" href="#plan">
+                                <i class="flaticon-sketch"></i>
+                                Planning &amp; Design
+                            </a>
+                        </li>
+
+                        <li>
+                            <a data-toggle="tab" href="#general">
+                                <i class="flaticon-architect-with-helmet"></i>
+                                General Constructing
+                            </a>
+                        </li>
+
+                        <li>
+                            <a data-toggle="tab" href="#manage">
+                                <i class="flaticon-engineer"></i>
+                                Construction Management
+                            </a>
+                        </li>
+
+                        <li>
+                            <a data-toggle="tab" href="#modeling">
+                                <i class="flaticon-skyline"></i>
+                                Building Modeling
+                            </a>
+                        </li>
+                    </ul>
+
+                    <div class="tab-content">
+
+                        <div id="plan" class="tab-pane fade in active">
+
+                            <div class="row">
+
+                                <div class="col-md-6">
+                                    <div class="services-img" style="background-image: url(images/building-1.jpg);"></div>
+                                </div>
+
+                                <div class="col-md-6">
+                                    <div class="services-desc">
+                                        <p>A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth. Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+
+                                        <ul>
+                                            <li>It is a paradisematic country in which</li>
+                                            <li>Even the all-powerful Pointing has no control</li>
+                                            <li>When she reached the first hills of the Italic Mountains</li>
+                                            <li>Alphabet Village and the subline of her own road</li>
+                                        </ul>
+                                    </div>
+                                </div>
+
+                            </div>
+
+                        </div>
+
+                        <div id="general" class="tab-pane fade">
+
+                            <div class="row">
+
+                                <div class="col-md-6">
+                                    <div class="services-img" style="background-image: url(images/building-2.jpg);"></div>
+                                </div>
+
+                                <div class="col-md-6">
+                                    <div class="services-desc">
+                                        <p>A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth. Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+
+                                        <ul>
+                                            <li>It is a paradisematic country in which</li>
+                                            <li>Even the all-powerful Pointing has no control</li>
+                                            <li>When she reached the first hills of the Italic Mountains</li>
+                                            <li>Alphabet Village and the subline of her own road</li>
+                                        </ul>
+                                    </div>
+                                </div>
+
+                            </div>
+                          
+                        </div>
+
+                        <div id="manage" class="tab-pane fade">
+
+                            <div class="row">
+
+                                <div class="col-md-6">
+                                    <div class="services-img" style="background-image: url(images/building-3.jpg);"></div>
+                                </div>
+
+                                <div class="col-md-6">
+                                    <div class="services-desc">
+                                        <p>A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth. Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+
+                                        <ul>
+                                            <li>It is a paradisematic country in which</li>
+                                            <li>Even the all-powerful Pointing has no control</li>
+                                            <li>When she reached the first hills of the Italic Mountains</li>
+                                            <li>Alphabet Village and the subline of her own road</li>
+                                        </ul>
+                                    </div>
+                                </div>
+
+                            </div>
+                           
+                        </div>
+
+                        <div id="modeling" class="tab-pane fade">
+
+                            <div class="row">
+
+                                <div class="col-md-6">
+                                    <div class="services-img" style="background-image: url(images/building-4.jpg);"></div>
+                                </div>
+
+                                <div class="col-md-6">
+                                    <div class="services-desc">
+                                        <p>A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth. Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+
+                                        <ul>
+                                            <li>It is a paradisematic country in which</li>
+                                            <li>Even the all-powerful Pointing has no control</li>
+                                            <li>When she reached the first hills of the Italic Mountains</li>
+                                            <li>Alphabet Village and the subline of her own road</li>
+                                        </ul>
+                                    </div>
+                                </div>
+
+                            </div>
+
+                       </div>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
 @endsection
