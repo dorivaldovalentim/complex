bfa0adc10e4ff0401dabd79abdb5b83364af55b3
Dorivaldo Valentim <dorivaldovalentim@gmail.com>
2021-01-27 03:11:39 +0100

    Adicionando a apresentação do menu no frontsite

---
+++ resources/views/components/main-menu.blade.php
@@ -0,0 +1,30 @@
+
+<nav class="colorlib-nav" role="navigation">
+    <div class="top-menu">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-2">
+                    <div id="colorlib-logo"><a href="index.html">Complex</a></div>
+                </div>
+                <div class="col-xs-10 text-right menu-1">
+                    <ul>
+                        <li class="active"><a href="index.html">Home</a></li>
+                        <li class="has-dropdown">
+                            <a href="project.html">Projects</a>
+                            <ul class="dropdown">
+                                <li><a href="#">Commercial</a></li>
+                                <li><a href="#">Apartment</a></li>
+                                <li><a href="#">House</a></li>
+                                <li><a href="#">Building</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="services.html">Services</a></li>
+                        <li><a href="blog.html">Blog</a></li>
+                        <li><a href="about.html">About</a></li>
+                        <li><a href="contact.html">Contact</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</nav>
