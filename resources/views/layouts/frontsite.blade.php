1232e10991561dc4737baa6eec04c4ac75c10274
Dorivaldo Valentim <dorivaldovalentim@gmail.com>
2021-01-27 23:36:16 +0100

    Adicionando a seção newsletter

--- resources/views/layouts/frontsite.blade.php
+++ resources/views/layouts/frontsite.blade.php
@@ -42,6 +42,56 @@
             <x-main-menu />
 
             @yield('content')
+
+            {{-- Newsletter Section --}}
+
+            <div id="colorlib-subscribe">
+
+                <div class="overlay"></div>
+
+                <div class="container">
+
+                    <div class="row">
+                        <div class="col-md-8 col-md-offset-2 text-center colorlib-heading animate-box">
+                            <h2>Subscribe Newsletter</h2>
+                            <p>Subscribe our newsletter and get latest update</p>
+                        </div>
+                    </div>
+
+                    <div class="row animate-box">
+
+                        <div class="col-md-6 col-md-offset-3">
+
+                            <div class="row">
+
+                                <div class="col-md-12">
+                                    <form class="form-inline qbstp-header-subscribe">
+                                        <div class="col-three-forth">
+                                            <div class="form-group">
+                                                <input type="text" class="form-control" id="email" placeholder="Enter your email">
+                                            </div>
+                                        </div>
+
+                                        <div class="col-one-third">
+                                            <div class="form-group">
+                                                <button type="submit" class="btn btn-primary">Subscribe Now</button>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+            {{-- End Newsletter Section --}}
+
         </div>
 
         <div class="gototop js-top">
