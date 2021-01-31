5a8ab79f370213fe918b91f4cc49de70fc93e9d9
Dorivaldo Valentim <dorivaldovalentim@gmail.com>
2021-01-31 17:27:24 +0100

    Mudando a posição dos itens no menu

--- resources/views/components/main-menu.blade.php
+++ resources/views/components/main-menu.blade.php
@@ -12,10 +12,10 @@
                 <div class="col-xs-10 text-right menu-1">
                     <ul>
                         <li><a href="{{ route('home') }}">Página Inicial</a></li>
-                        <li><a href="{{ route('projects') }}">Projectos</a></li>
-                        <li><a href="{{ route('services') }}">Serviços</a></li>
-                        <li><a href="{{ route('blog') }}">Blog</a></li>
                         <li><a href="{{ route('about') }}">Sobre</a></li>
+                        <li><a href="{{ route('services') }}">Serviços</a></li>
+                        <li><a href="{{ route('projects') }}">Projectos</a></li>
+                        <li><a href="{{ route('blog') }}">Blog</a></li>
                         <li><a href="{{ route('contacts') }}">Contactos</a></li>
                     </ul>
                 </div>
