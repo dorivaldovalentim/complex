6447fcd877e67b6e6f964e16c0a422bdd8d7fbb4
Dorivaldo Valentim <dorivaldovalentim@gmail.com>
2021-01-31 16:39:48 +0100

    Adding the project page

---
+++ resources/views/frontsite/projects.blade.php
@@ -0,0 +1,73 @@
+@extends('layouts.frontsite')
+
+@section('title', 'Projectos')
+
+@section('content')
+
+    <aside id="colorlib-hero">
+
+        <div class="flexslider">
+
+            <ul class="slides">
+
+                <li style="background-image: url(images/cover_img_3.jpg);">
+
+                    <div class="overlay"></div>
+
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="col-md-8 col-sm-12 col-md-offset-2 slider-text">
+                                <div class="slider-text-inner text-center">
+                                    <h1>Projectos</h1>
+                                    <h2>
+                                        <a href="{{ route('home') }}">Complex</a>
+                                        » Projectos
+                                    </h2>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </li>
+
+            </ul>
+
+        </div>
+
+    </aside>
+
+    <div id="colorlib-project">
+
+        <div class="container">
+
+            <div class="row">
+
+                <div class="col-md-4 animate-box">
+
+                    <div class="project-entry">
+
+                        <a href="#" class="project-img text-center" style="background-image: url(images/building-1.jpg);">
+                            <span class="icon">
+                                <i class="icon-search2"></i>
+                            </span>
+                        </a>
+
+                        <div class="desc">
+                            
+                            <h3>
+                                <a href="#">Apartment Interior Design</a>
+                            </h3>
+                        
+                        </div>
+
+                    </div>
+
+                </div>
+                
+            </div>
+
+        </div>
+
+    </div>
+
+@endsection
